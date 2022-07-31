64fa99e7a2357bec9ca535a44c4ce65733398d73
Carolina <cindy.s.r.28@hotmail.com>
2022-07-31 17:41:01 -0500

    fix HOC withStyles

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
-import "./App.css";
+import './App.css';
 
-function withStyles(Component) {
-  return (props) => {
-    console.log("here", <Component />);
-    const style = { padding: "0.2rem", color: "blue" };
-    return <Component style={style} {...props} />;
-  };
-}
+const withStyles = (Component) => (props) => {
+  console.log('props', props);
+  const style = { padding: '0.2rem', color: 'blue' };
+  return <Component style={style} {...props} />;
+};
 
-const Button = () => <button>Click me!</button>;
-const Text = () => <p>Hello World!</p>;
+const Button = (props) => <button {...props}>Click me!</button>;
+const Text = (props) => <p {...props}>Hello World!</p>;
 
 const StyledButton = withStyles(Button);
 const StyledText = withStyles(Text);
 
 function App() {
   return (
-    <div className="App">
-      <StyledButton />
+    <div className='App'>
+      <StyledButton onClick={() => console.log('clicked')} />
       <StyledText />
     </div>
   );
